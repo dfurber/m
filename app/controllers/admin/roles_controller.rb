fdf7f4e1e96b1985846becf2fba6ba5a19983aea
David <furberd@gmail.com>
2010-12-14 16:35:19 -0500

    removing unneeded stuff, adding login layout

--- app/controllers/admin/roles_controller.rb
+++ app/controllers/admin/roles_controller.rb
@@ -5,6 +5,10 @@ class Admin::RolesController < Admin::BaseController
   collection_table do |t|
     t.row_actions :edit_role_link, :edit_permissions_link, :destroy_role_link
   end
+  
+  resource_form do |f|
+    f.input :name
+  end
 
   def remove
     @role = Role.find params[:id]
--- app/controllers/blog_categories_controller.rb
+++
@@ -1,5 +0,0 @@
-class BlogCategoriesController < ApplicationController
-  resourcify
-  actions :all, :except => [:show]
-  layout 'admin'
-end
